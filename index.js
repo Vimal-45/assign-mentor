11ee62b2507ff27be2b8bff5e2f75e583b848058
Vimal-45 <dvimal45@gmail.com>
2023-11-09 22:36:46 +0530

    add API list

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.get('/', (req, res) => {
       <ul>
         <li><a href="https://frontend-assign-mentor.vercel.app/">Please Click here to send and post data</a></li>        
       </ul> 
+      <ul>
+      <li>Get Mentor List - https://assign-mentor-9g6m.onrender.com/api/get/mentors</li>
+      <li>Get Student List - https://assign-mentor-9g6m.onrender.com/api/get/student</li>
+      <li>Get Assigned Student List - https://assign-mentor-9g6m.onrender.com/api/previous/data</li>
+      <li>Get Updated Mentor List - https://assign-mentor-9g6m.onrender.com/api/updated/data</li>
+      <li>Create Mentor - https://assign-mentor-9g6m.onrender.com/api/create/mentors</li>
+      <li>Create Student - https://assign-mentor-9g6m.onrender.com/create/student</li>
+      <li>Assign Student - https://assign-mentor-9g6m.onrender.com/api/assign/student</li>
+      <li>Update Mentor - https://assign-mentor-9g6m.onrender.com/api/change/mentor</li>
+    </ul>
     `;
     res.send(`The assign-Mentor app is working <br><br>${frontend}`);
   });
